48fb792d5e876c4170221048ddcfb311d5d43cee
imacatpro <157082469+imacatpro@users.noreply.github.com>
2025-02-25 06:52:54 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="prc-sdk",
+    name="prc_sdk",
     version="0.1.0",
     author="zenturocloud",
     author_email="info@zenturo.cloud",
